4cbf878ddaf7d53fc5f5affcbec606dcee214277
praveen1438 <127485994+praveen1438@users.noreply.github.com>
2023-03-10 16:18:57 +0530

    Add files via upload

---
+++ additionoftwonumbers.java
@@ -0,0 +1,18 @@
+package com.additionoftwonumbers;
+
+import java.util.Scanner;
+
+public class additionoftwonumbers {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int a,b,c;
+        System.out.println("enter  a A value");
+        a=s.nextInt();
+        System.out.println("enter  a A value");
+        b=s.nextInt();
+        c=a+b;
+        System.out.println("Addition of two numbers is:"+c);
+    }
+
+
+}
